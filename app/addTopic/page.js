7bdad6b2b0a28be864f8196febb5c6dceba4e0a4
i-adarsh <i.adarsh.kr@icloud.com>
2025-06-01 17:54:20 +0000

    git add topic

--- app/addTopic/page.js
+++ app/addTopic/page.js
@@ -1,12 +1,61 @@
-'use client';
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function AddTopic() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-    return (
-        <form className="flex flex-col gap-3">
-            <input className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic title"></input>
-            <input className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic description"></input>
-            <button className="bg-blue-500 hover:bg-blue-600 font-bold text-white py-3 px-6 w-fir rounded-xl">Add Topic</button>
-        </form>
-    );
+  const router = useRouter();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(title, description);
+    if (!title || !description) {
+      alert("Please enter a title and description");
+    } else {
+      try {
+        const res = await fetch("http://localhost:3000/api/topics", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ title, description }),
+        });
+
+        if (res.ok) {
+          router.push("/");
+        } else {
+          throw new Error("failed to add topic");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+      <input
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic title"
+      />
+      <input
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic description"
+      />
+      <button
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-600 font-bold text-white py-3 px-6 w-fir rounded-xl"
+      >
+        Add Topic
+      </button>
+    </form>
+  );
 }
