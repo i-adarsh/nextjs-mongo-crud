51160663f5a01139c024d5206df84c6361863690
i-adarsh <i.adarsh.kr@icloud.com>
2025-06-02 12:52:51 +0000

    updated deleted feature

--- components/RemoveBtn.js
+++ components/RemoveBtn.js
@@ -8,17 +8,13 @@ export default function RemoveBtn({ id }) {
   const removeTopic = async () => {
     const confirmed = confirm("Are you sure?");
     if (confirmed) {
-      const res = await fetch(
-        `${process.env.PUBLIC_URL}/api/topics?id=${id}`,
-        {
-          method: "DELETE",
-        }
-      );
+      const res = await fetch(`/api/topics?id=${id}`, {
+        method: "DELETE",
+      });
       if (res.ok) {
         router.refresh();
       }
     }
-
   };
   return (
     <button onClick={removeTopic} className="text-red-400">
